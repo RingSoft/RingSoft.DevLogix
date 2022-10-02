9d17849518426a332d9db7870b2c533af8115aec
Peter Ringering <peteman316@gmail.com>
2022-10-02 12:55:43 -0600

    Add project files.

---
+++ RingSoft.DevLogix/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace RingSoft.DevLogix
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
