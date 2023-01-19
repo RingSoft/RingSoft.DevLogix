e8392be94e85bc098ebec7b125268619f6c84576
Peter Ringering <peteman316@gmail.com>
2023-01-18 17:26:05 -0700

    Working

--- RingSoft.DevLogix.Library/AppGlobals.cs
+++ RingSoft.DevLogix.Library/AppGlobals.cs
@@ -57,7 +57,7 @@ namespace RingSoft.DevLogix.Library
             RingSoftAppGlobals.AppCopyright = "©2023 by Peter Ringering";
             RingSoftAppGlobals.PathToDownloadUpgrade = MasterDbContext.ProgramDataFolder;
             RingSoftAppGlobals.AppGuid = "96ac9aa5-65af-43ca-8cb8-6e35a2f12570";
-            RingSoftAppGlobals.AppVersion = 19;
+            RingSoftAppGlobals.AppVersion = 20;
         }
 
         public static void Initialize()
--- RingSoft.DevLogix/InstallScript.iss
+++ RingSoft.DevLogix/InstallScript.iss
@@ -2,7 +2,7 @@
 ; SEE THE DOCUMENTATION FOR DETAILS ON CREATING INNO SETUP SCRIPT FILES!
 
 #define MyAppName "DevLogix"
-#define MyAppVersion "00.85.7"
+#define MyAppVersion "00.85.8"
 #define MyAppPublisher "RingSoft"
 #define MyAppURL "http://www.ringsoft.site/"
 #define MyAppExeName "RingSoft.DevLogix.exe"
