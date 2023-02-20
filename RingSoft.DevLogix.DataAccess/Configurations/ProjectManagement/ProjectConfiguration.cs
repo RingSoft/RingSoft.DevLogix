d4554042217388ca4fbfd05d1adf5e4378866048
Peter Ringering <peteman316@gmail.com>
2023-02-19 23:00:02 -0700

    Working

--- RingSoft.DevLogix.DataAccess/Configurations/ProjectManagement/ProjectConfiguration.cs
+++ RingSoft.DevLogix.DataAccess/Configurations/ProjectManagement/ProjectConfiguration.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RingSoft.DbLookup.EfCore;
+using RingSoft.DevLogix.DataAccess.Model.ProjectManagement;
 
 namespace RingSoft.DevLogix.DataAccess.Configurations.ProjectManagement
 {
-    internal class ProjectConfiguration
+    public class ProjectConfiguration : IEntityTypeConfiguration<Project>
     {
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+            builder.Property(p => p.Id).HasColumnType(DbConstants.IntegerColumnType);
+            builder.Property(p => p.Name).HasColumnType(DbConstants.StringColumnType);
+            builder.Property(p => p.Deadline).HasColumnType(DbConstants.DateColumnType);
+        }
     }
 }
