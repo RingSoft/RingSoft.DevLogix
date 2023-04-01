4ab8c7a38245ef6dab1b2749535a60e6ddbeb229
Peter Ringering <peteman316@gmail.com>
2023-03-31 22:59:54 -0600

    Working

---
+++ RingSoft.DevLogix.DataAccess/Model/QualityAssurance/TestingTemplate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RingSoft.DevLogix.DataAccess.Model.QualityAssurance
+{
+    public class TestingTemplate
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public int? BaseTemplateId { get; set; }
+
+        public virtual TestingTemplate BaseTemplate { get; set; }
+
+        public string? Notes { get; set; }
+
+        public virtual ICollection<TestingTemplate> ChildTemplates { get; set; }
+
+        public TestingTemplate()
+        {
+            ChildTemplates = new HashSet<TestingTemplate>();
+        }
+    }
+}
