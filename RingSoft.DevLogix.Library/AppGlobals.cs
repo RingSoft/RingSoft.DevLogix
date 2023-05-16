9a4983a586ddd7218266738f00044f03bec379d9
Peter Ringering <peteman316@gmail.com>
2023-05-15 23:33:22 -0600

    Working

--- RingSoft.DevLogix.Library/AppGlobals.cs
+++ RingSoft.DevLogix.Library/AppGlobals.cs
@@ -68,7 +68,7 @@ namespace RingSoft.DevLogix.Library
             RingSoftAppGlobals.AppCopyright = "©2023 by Peter Ringering";
             RingSoftAppGlobals.PathToDownloadUpgrade = MasterDbContext.ProgramDataFolder;
             RingSoftAppGlobals.AppGuid = "96ac9aa5-65af-43ca-8cb8-6e35a2f12570";
-            RingSoftAppGlobals.AppVersion = 98;
+            RingSoftAppGlobals.AppVersion = 106;
             SystemGlobals.ProgramDataFolder = MasterDbContext.ProgramDataFolder;
         }
 
