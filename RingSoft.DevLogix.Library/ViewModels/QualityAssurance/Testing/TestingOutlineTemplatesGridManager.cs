49d7d50c596eda13cfc7137d179b2e8c6f0cccfb
Peter Ringering <peteman316@gmail.com>
2023-05-21 14:42:56 -0600

    Working

--- RingSoft.DevLogix.Library/ViewModels/QualityAssurance/Testing/TestingOutlineTemplatesGridManager.cs
+++ RingSoft.DevLogix.Library/ViewModels/QualityAssurance/Testing/TestingOutlineTemplatesGridManager.cs
@@ -56,6 +56,7 @@ namespace RingSoft.DevLogix.Library.ViewModels.QualityAssurance.Testing
                             , testingOutline
                             , startIndex);
                     details.AddRange(generatedDetails);
+                    startIndex += generatedDetails.Count;
                 }
             }
 
