5e2dbd4daca47efe75b499f156f4688346b696b3
Peter Ringering <peteman316@gmail.com>
2023-05-23 14:46:34 -0600

    Working

--- RingSoft.DevLogix.Tests/UserManagement/UserTests.cs
+++ RingSoft.DevLogix.Tests/UserManagement/UserTests.cs
@@ -4,6 +4,7 @@ using RingSoft.DbLookup;
 using RingSoft.DbMaintenance;
 using RingSoft.DevLogix.DataAccess.Model;
 using RingSoft.DevLogix.DataAccess.Model.ProjectManagement;
+using RingSoft.DevLogix.DataAccess.Model.QualityAssurance;
 using RingSoft.DevLogix.Library;
 using RingSoft.DevLogix.Library.ViewModels.UserManagement;
 using RingSoft.DevLogix.Sqlite;
@@ -47,6 +48,49 @@ namespace RingSoft.DevLogix.Tests.UserManagement
             Assert.IsTrue(autoFillValue.IsValid());
             Globals.ViewModel.DepartmentAutoFillValue = autoFillValue;
             Assert.IsTrue(Globals.ViewModel.RecordDirty);
+
+            Globals.ViewModel.NewCommand.Execute(null);
+            var chart = new DevLogixChart()
+            {
+                Id = 1,
+                Name = "Test",
+            };
+            autoFillValue = chart.GetAutoFillValue();
+            Assert.IsTrue(autoFillValue.IsValid());
+            Globals.ViewModel.DefaultChartAutoFillValue = autoFillValue;
+            Assert.IsTrue(Globals.ViewModel.RecordDirty);
+
+            Globals.ViewModel.NewCommand.Execute(null);
+            autoFillValue = user.GetAutoFillValue();
+            Assert.IsTrue(autoFillValue.IsValid());
+            Globals.ViewModel.SupervisorAutoFillValue = autoFillValue;
+            Assert.IsTrue(Globals.ViewModel.RecordDirty);
+
+            Globals.ViewModel.NewCommand.Execute(null);
+            var clockDate = new DateTime(2000, 1, 1);
+            Globals.ViewModel.ClockDateTime = clockDate;
+            Assert.IsFalse(Globals.ViewModel.RecordDirty);
+
+            Globals.ViewModel.NewCommand.Execute(null);
+            var clockReason = "Test";
+            Globals.ViewModel.ClockReason = clockReason;
+            Assert.IsFalse(Globals.ViewModel.RecordDirty);
+
+            Globals.ViewModel.NewCommand.Execute(null);
+            Globals.ViewModel.EmailAddress = "Test";
+            Assert.IsTrue(Globals.ViewModel.RecordDirty);
+
+            Globals.ViewModel.NewCommand.Execute(null);
+            Globals.ViewModel.PhoneNumber = "Test";
+            Assert.IsTrue(Globals.ViewModel.RecordDirty);
+
+            Globals.ViewModel.NewCommand.Execute(null);
+            Globals.ViewModel.HourlyRate = 40;
+            Assert.IsTrue(Globals.ViewModel.RecordDirty);
+
+            Globals.ViewModel.NewCommand.Execute(null);
+            Globals.ViewModel.Notes = "Test";
+            Assert.IsTrue(Globals.ViewModel.RecordDirty);
         }
 
         [TestMethod]
@@ -57,12 +101,13 @@ namespace RingSoft.DevLogix.Tests.UserManagement
             {
                 Id = 1,
                 Name = "Test",
+                HourlyRate = 10,
             };
 
             var timeClock = new TimeClock()
             {
                 Id = 1,
-                MinutesSpent = 10,
+                MinutesSpent = 60,
                 ProjectTask = new ProjectTask()
                 {
                     Id = 1,
@@ -76,12 +121,55 @@ namespace RingSoft.DevLogix.Tests.UserManagement
                 },
                 ProjectTaskId = 1,
             };
+            user.TimeClocks.Add(timeClock);
+
+            timeClock = new TimeClock()
+            {
+                Id = 2,
+                MinutesSpent = 120,
+                ProjectTask = new ProjectTask()
+                {
+                    Id = 2,
+                    Name = "Test2",
+                    Project = new Project()
+                    {
+                        Id = 2,
+                        Name = "Test2",
+                        IsBillable = false,
+                    },
+                },
+                ProjectTaskId = 2,
+            };
+            user.TimeClocks.Add(timeClock);
+
+            timeClock = new TimeClock()
+            {
+                Id = 3,
+                MinutesSpent = 180,
+                Error = new Error()
+                {
+                    Id = 1,
+                },
+                ErrorId = 1,
+            };
+            user.TimeClocks.Add(timeClock);
+
+            timeClock = new TimeClock()
+            {
+                Id = 4,
+                MinutesSpent = 240,
+                TestingOutline = new TestingOutline(),
+                TestingOutlineId = 1,
+            };
 
             user.TimeClocks.Add(timeClock);
 
             Globals.DataRepository.DataContext.SaveNoCommitEntity(user, "Test");
             Globals.ViewModel.RecalcCommand.Execute(null);
-            Assert.IsTrue(user.BillableProjectsMinutesSpent == 10);
+            Assert.IsTrue(user.BillableProjectsMinutesSpent == 60);
+            Assert.IsTrue(user.NonBillableProjectsMinutesSpent == 120);
+            Assert.IsTrue(user.ErrorsMinutesSpent == 180);
+            Assert.IsTrue(user.TestingOutlinesMinutesSpent == 240);
         }
     }
 }
