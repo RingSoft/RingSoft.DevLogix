3ae89e65aa7fd0b7b928bbedbfc861cca0e9c76d
Peter Ringering <peteman316@gmail.com>
2023-07-05 05:51:10 -0600

    Working

--- RingSoft.DevLogix.Library/ViewModels/CustomerManagement/CustomerComputerViewModel.cs
+++ RingSoft.DevLogix.Library/ViewModels/CustomerManagement/CustomerComputerViewModel.cs
@@ -56,6 +56,36 @@ namespace RingSoft.DevLogix.Library.ViewModels.CustomerManagement
             }
         }
 
+        private string? _brand;
+
+        public string? Brand
+        {
+            get => _brand;
+            set
+            {
+                if (_brand == value)
+                    return;
+
+                _brand = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string? _operatingSystem;
+
+        public string? OperatingSystem
+        {
+            get => _operatingSystem;
+            set
+            {
+                if (_operatingSystem == value)
+                    return;
+
+                _operatingSystem = value;
+                OnPropertyChanged();
+            }
+        }
+
         private double? _speed;
 
         public double? Speed
@@ -87,6 +117,109 @@ namespace RingSoft.DevLogix.Library.ViewModels.CustomerManagement
             }
         }
 
+        private int? _hardDriveSize;
+
+        public int? HardDriveSize
+        {
+            get => _hardDriveSize;
+            set
+            {
+                if (_hardDriveSize == value)
+                    return;
+
+                _hardDriveSize = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int? _hardDriveFree;
+
+        public int? HardDriveFree
+        {
+            get => _hardDriveFree;
+            set
+            {
+                if (_hardDriveFree == value)
+                    return;
+
+                _hardDriveFree = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string? _screenResolution;
+
+        public string? ScreenResolution
+        {
+            get => _screenResolution;
+            set
+            {
+                if (_screenResolution == value)
+                    return;
+
+                _screenResolution = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int? _internetSpeed;
+
+        public int? InternetSpeed
+        {
+            get => _internetSpeed;
+            set
+            {
+                if (_internetSpeed == value)
+                    return;
+
+                _internetSpeed = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string? _databasePlatform;
+
+        public string? DatabasePlatform
+        {
+            get => _databasePlatform;
+            set
+            {
+                if (_databasePlatform == value)
+                    return;
+
+                _databasePlatform = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string? _printer;
+
+        public string? Printer
+        {
+            get => _printer;
+            set
+            {
+                if (_printer == value)
+                    return;
+
+                _printer = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string? _notes;
+
+        public string? Notes
+        {
+            get => _notes;
+            set
+            {
+                if (_notes == value) return;
+
+                _notes = value;
+                OnPropertyChanged();
+            }
+        }
 
         public CustomerComputerViewModel()
         {
@@ -109,8 +242,17 @@ namespace RingSoft.DevLogix.Library.ViewModels.CustomerManagement
         protected override void LoadFromEntity(CustomerComputer entity)
         {
             CustomerAutoFillValue = entity.Customer.GetAutoFillValue();
+            Brand = entity.Brand;
+            OperatingSystem = entity.OperatingSystem;
             Speed = entity.Speed;
             RamSize = entity.RamSize;
+            HardDriveSize = entity.HardDriveSize;
+            HardDriveFree = entity.HardDriveFree;
+            ScreenResolution = entity.ScreenResolution;
+            InternetSpeed = entity.InternetSpeed;
+            DatabasePlatform = entity.DatabasePlatform;
+            Printer = entity.Printer;
+            Notes = entity.Notes;
         }
 
         protected override CustomerComputer GetEntityData()
@@ -120,8 +262,17 @@ namespace RingSoft.DevLogix.Library.ViewModels.CustomerManagement
                 Id = Id,
                 Name = KeyAutoFillValue?.Text,
                 CustomerId = CustomerAutoFillValue.GetEntity<Customer>().Id,
+                Brand = Brand,
+                OperatingSystem = OperatingSystem,
                 Speed = Speed,
                 RamSize = RamSize,
+                HardDriveSize = HardDriveSize,
+                HardDriveFree = HardDriveFree,
+                ScreenResolution = ScreenResolution,
+                InternetSpeed = InternetSpeed,
+                DatabasePlatform = DatabasePlatform,
+                Printer = Printer,
+                Notes = Notes,
             };
         }
 
@@ -130,8 +281,17 @@ namespace RingSoft.DevLogix.Library.ViewModels.CustomerManagement
             Id = 0;
             KeyAutoFillValue = null;
             CustomerAutoFillValue = null;
+            Brand = null;
+            OperatingSystem = null;
             Speed = null;
             RamSize = null;
+            HardDriveSize = null;
+            HardDriveFree = null;
+            ScreenResolution = null;
+            InternetSpeed = null;
+            DatabasePlatform = null;
+            Printer = null;
+            Notes = null;
         }
 
         protected override bool SaveEntity(CustomerComputer entity)
