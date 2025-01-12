c25b21469a0f58ad60155c8706ca37be333a44f7
Peter Ringering <peteman316@gmail.com>
2025-01-11 20:09:16 -0700

    Working

---
+++ RingSoft.DevLogix.Library/ViewModels/QualityAssurance/Testing/TestingOutlineRetestFilterViewModel.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using RingSoft.DataEntryControls.Engine;
+using RingSoft.DbLookup.AutoFill;
+
+namespace RingSoft.DevLogix.Library.ViewModels.QualityAssurance.Testing
+{
+    public enum RetestFilterTypes
+    {
+        Outline = 0,
+        Product = 1,
+    }
+
+    public class TestingOutlineRetestFilterViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+
+        private RetestFilterTypes _retestFilterType;
+
+        public RetestFilterTypes RetestFilterType
+        {
+            get { return _retestFilterType; }
+            set
+            {
+                if (_retestFilterType == value)
+                {
+                    return;
+                }
+                _retestFilterType = value;
+                OnPropertyChanged();
+                SetVisuals();
+            }
+        }
+
+        private AutoFillSetup _startOutlineAutoFillSetup;
+
+        public AutoFillSetup StartOutlineAutoFillSetup
+        {
+            get { return _startOutlineAutoFillSetup; }
+            set
+            {
+                if (_startOutlineAutoFillSetup == value)
+                    return;
+
+                _startOutlineAutoFillSetup = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private AutoFillValue _startOutlineAutoFillValue;
+
+        public AutoFillValue StartOutlineAutoFillValue
+        {
+            get { return _startOutlineAutoFillValue; }
+            set
+            {
+                if (_startOutlineAutoFillValue == value)
+                    return;
+
+                _startOutlineAutoFillValue = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        #endregion
+
+        public UiCommand StartOutlineUiCommand { get; }
+
+        public UiCommand EndOutlineUiCommand { get; }
+
+        public UiCommand StartProductUiCommand { get; }
+
+        public UiCommand EndProductUiCommand { get; }
+
+        public TestingOutlineRetestFilterViewModel()
+        {
+            StartOutlineUiCommand = new UiCommand();
+            EndOutlineUiCommand = new UiCommand();
+            StartProductUiCommand = new UiCommand();
+            EndProductUiCommand = new UiCommand();
+
+            StartOutlineAutoFillSetup = new AutoFillSetup(AppGlobals.LookupContext.TestingOutlineLookup);
+            StartOutlineAutoFillSetup.AllowLookupAdd = StartOutlineAutoFillSetup.AllowLookupView = false;
+        }
+
+        public void Initialize()
+        {
+            SetVisuals();
+        }
+
+        private void SetVisuals()
+        {
+            StartOutlineUiCommand.IsEnabled = RetestFilterType == RetestFilterTypes.Outline;
+            EndOutlineUiCommand.IsEnabled = RetestFilterType == RetestFilterTypes.Outline;
+            StartProductUiCommand.IsEnabled = RetestFilterType == RetestFilterTypes.Product;
+            EndProductUiCommand.IsEnabled = RetestFilterType == RetestFilterTypes.Product;
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
--- RingSoft.DevLogix/QualityAssurance/TestingOutlineRetestFilterWindow.xaml.cs
+++ RingSoft.DevLogix/QualityAssurance/TestingOutlineRetestFilterWindow.xaml.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-namespace RingSoft.DevLogix.QualityAssurance
+﻿namespace RingSoft.DevLogix.QualityAssurance
 {
     /// <summary>
     /// Interaction logic for TestingOutlineRetestFilterWindow.xaml
@@ -22,6 +8,11 @@ namespace RingSoft.DevLogix.QualityAssurance
         public TestingOutlineRetestFilterWindow()
         {
             InitializeComponent();
+
+            Loaded += (sender, args) =>
+            {
+                ViewModel.Initialize();
+            };
         }
     }
 }
